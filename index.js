2a3c4f7849502dd4deb1abb7979dbe1c1a576c38
Barshon <barshonweb@gmail.com>
2024-05-30 11:58:17 +0600

    data insert by node and mongodb

--- index.js
+++ index.js
@@ -1,47 +1,57 @@
 //server
-const express= require('express');
-const cors=require('cors');
-const env=require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb')
-const app=express();
-const port=3100;
+const express = require("express");
+const cors = require("cors");
+const env = require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = 3100;
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("server running");
+});
 
-app.get('/',(req,res)=>{
-    res.send('server running');
-})
-
-
-
-const uri =`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@picsamaze.cninswl.mongodb.net/?retryWrites=true&w=majority&appName=PicsAmaze"`;;
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@picsamaze.cninswl.mongodb.net/?retryWrites=true&w=majority&appName=PicsAmaze"`;
 
 // Create a new client and connect to MongoDB
 const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
 
-    async function run() {
-        try {
-          await client.connect();
-          await client.db("admin").command({ ping: 1 });
-          console.log("Pinged your deployment. You successfully connected to MongoDB!");
-        } finally {
-          await client.close();
-        }
-      }
-      run().catch(console.dir);
+    const picsAmazedb = client.db("picsAmazeDB");
+    const picsCollection = picsAmazedb.collection("picsCollection");
 
+    app.post("/pics", async(req,res) => {
+      //data
+      
+      const picInfo=req.body;
+      const result= await picsCollection.insertOne(picInfo);
+      res.send(result); 
+      console.log(`a document has been inserted, id: ${result.insertedId}`);
+    });
+    //
+    
+  } finally {
+    //   await client.close();
+  }
+}
+run().catch(console.dir);
 
-
-app.listen(port,()=>{
-    console.log(`listening at port ${port}`);
-} );
+app.listen(port, () => {
+  console.log(`listening at port ${port}`);
+});
